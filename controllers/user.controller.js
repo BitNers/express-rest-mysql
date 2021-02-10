b9fc851bf9b2618a8383bd8e094d80bee47346d1
BitNers <wesleyalvessilva@outlook.com.br>
2021-02-10 19:18:38 -0300

    Updating User Controller
    Importing the BCrypt to hash passwords and updating some functions, and adding Catch callbacks for errors treatments.
    Furthermore, updating the response standard for user, such as 'ok' or 'error' with a message.

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,32 +1,34 @@
+const bcrypt = require('bcrypt');
 const db = require('../database/models');
 const User = db.User;
 
 exports.findAllUsers = (req, res) => {
-
-   User.findAll({attributes: {exclude: ['passwd']}}).then(users=>{
-       res.json(users);
-   })
+   User.findAll({attributes: {exclude: ['passwd']}})
+   .then(users=>{res.json(users);})
+   .catch(err=>{res.status(500).json({status: "error", message: err || "Something went wrong while try to find all users."})})
 };
 
-exports.findByName = (req,res)=>{
-    const name = req.params.name;
+exports.findById = (req,res)=>{
+    const id = req.params.id;
     User.findOne({
-        where: {username: name}, 
+        where: {id_user: id}, 
         attributes: {
-         exclude: ['passwd']}})
-         .then(data=>{res.json(data)}).catch(err=>{res.status(500).send("Error while finding the user.")});
+            exclude: ['passwd']}})
+         .then(data=>{res.json(data)})
+         .catch(err=>{res.status(500).json({status: "error", message: err || "Something went wrong while try to find user by name."})});
 }
 
 exports.createUser = (req,res)=>{
-    if (!req.body.username || !req.body.passwd) {
-        res.status(400).json({status: "err",message: "User and Password cannot be empty!"
-        });
+    if (!req.body.username || !req.body.passwd || !req.body.email) {res.status(400).json({status: "err",message: "User and Password cannot be empty!"});
         return;
       }
       const user = {
           username: req.body.username,
-          passwd: req.body.passwd,
+          passwd: bcrypt.hashSync(req.body.passwd, 8),
+          email: req.body.email
       }
 
-      User.create(user).then(data=>{res.json({status: "ok", message: "User created."})});
+      User.create(user)
+      .then(data=>{res.json({status: "ok", message: "User was created."})})
+      .catch(err=>{res.status(500).json({status: "error", message: err || "Something went wrong while creating the new user"})});
 }
