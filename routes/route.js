5516b6d13fc1ce6f658a69cc8dfc719732b0d286
BitNers <wesleyalvessilva@outlook.com.br>
2021-02-10 19:13:57 -0300

    Update route.js
    Updating some responses in root path, and changing the search query by name to ID.

--- routes/route.js
+++ routes/route.js
@@ -2,13 +2,12 @@ module.exports = (app)=>{
     const userControl = require("../controllers/user.controller");
     
     app.get('/', async (req,res,next)=>{
-        res.json({status: true});
+        res.json({status: "ok", message: "It's working."});
         next();
     })
 
     app.post('/', userControl.createUser)
     app.get('/users', userControl.findAllUsers)
-    app.get('/users/:name', userControl.findByName)
-    // app.get('/users/:id', userControl.encontrarPorId)
+    app.get('/users/:id', userControl.findById)
 
 }
