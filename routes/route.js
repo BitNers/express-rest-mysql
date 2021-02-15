2ffcdc5a31300a497c8f2aabcdd6bdc781db7a42
BitNers <wesleyalvessilva@outlook.com.br>
2021-02-15 01:06:44 -0300

    Updating Routes for new User Controller.

--- routes/route.js
+++ routes/route.js
@@ -1,12 +1,12 @@
 module.exports = (app)=>{
     const userControl = require("../controllers/user.controller");
-    
-    app.get('/', async (req,res,next)=>{
-        res.json({status: "ok", message: "It's working."});
-        next();
-    })
+    const userLogin = require("../controllers/auth.controller");
+
+    app.get('/', async (req,res,next)=>{res.json({status: "ok", message: "It's working."});})
 
     app.post('/', userControl.createUser)
+    app.post('/login', userLogin.validateLogin)
+
     app.get('/users', userControl.findAllUsers)
     app.get('/users/:id', userControl.findById)
 
