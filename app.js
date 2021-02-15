76fa9edee8d50b9ef92994385b9e02baf3e3247f
BitNers <wesleyalvessilva@outlook.com.br>
2021-02-15 20:49:07 -0300

    Update app.js
    Updating app, adding cookie-parser in App and using dotenv port. Updating route for user.

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const exp = require('express');
 const app = exp();
 const bodyparser = require('body-parser');
-require("dotenv-safe").config();
-
-let port = 3000
+const cookies = require('cookie-parser');
+require('dotenv').config();
 
 app.use(bodyparser.json());
+app.use(cookies());
 
+require('./routes/home.route')(app);
+require('./routes/user.route')(app);
 
-require('./routes/route')(app);
-
-app.listen(port, ()=>{
-    console.log(`Running in http://127.0.0.1:${port}`)
+app.listen(process.env.PORT, ()=>{
+    console.log(`Running in http://127.0.0.1:${process.env.PORT}`)
 });
