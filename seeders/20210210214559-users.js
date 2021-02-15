6420d3a5844dc3b61c76e8a220f9e75e2a8db4ce
BitNers <wesleyalvessilva@outlook.com.br>
2021-02-15 01:05:37 -0300

    Adding new seed for testing.

--- seeders/20210210214559-users.js
+++ seeders/20210210214559-users.js
@@ -24,6 +24,11 @@ module.exports = {
       email: 'j0ker@arthur.ch',
       passwd: '$2b$08$J1GgFmkGMZME/NMVnPveSu9IMGOpTGoscnXanFrpqv1GhgNNf8Wei',
     },
+    {
+      username: 'Wesley',
+      email: 'adedonha@adedonha.com.br',
+      passwd: '$2b$08$gS1eH2ZmcW8/b8Lmo5ueT.u2yyuKvSSVKf3lb5LXkBZfjipvB3Uzu',
+    },
   ], {}),
   down:  (queryInterface, Sequelize) => queryInterface.bulkDelete('users', null, {}),
 };
