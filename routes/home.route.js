90832ba6ab885f080d2cf99f38a21c8287ae0f0a
BitNers <wesleyalvessilva@outlook.com.br>
2021-02-16 17:34:36 -0300

    Update home route
    Passing data to process inside the view. Just testing.

--- routes/home.route.js
+++ routes/home.route.js
@@ -2,8 +2,7 @@ module.exports = (app)=>{
     const guestControl = require("../controllers/guest.controller");
 
     // Access Home Page
-    app.get('/', async (req,res,next)=>{res.json({status: "ok", message: "Home Page."});})
-
+    app.get('/', async (req,res)=>{res.render('pages/home', {title: "Home", data: "Hearts/Wires (In the House of Flies)"})})
     // Create new User
     app.post('/', guestControl.createUser)
 
