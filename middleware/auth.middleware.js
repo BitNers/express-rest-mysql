9f4f706ea2984314ba49ef2d3c49e0e96d3dcd01
BitNers <wesleyalvessilva@outlook.com.br>
2021-02-16 11:33:33 -0300

    Updating Auth Middleware
    Updating old JWT method for auto-login using token, now session is stored in Database.

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,16 +1,5 @@
-const jwt = require('jsonwebtoken');
-const db = require('../database/models');
-const User = db.User;
-
 exports.alreadyLogged = async (req,res,next)=>{
-    let token = await User.findOne({
-        where: {token: req.cookies['access-token']},
-        attributes: {
-            exclude: ['id_user', 'email', 'passwd', 'username', 'createdAt', 'updatedAt','role']}
-    });   
-
-    if(token.token == '')
-        return res.redirect('/');
+    if(!req.session.email)
+        return res.status('403').redirect('/');
     next();
-
 };
