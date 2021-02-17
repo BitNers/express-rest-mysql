755500949e840db177ecab467d3570aa27f2868f
BitNers <wesleyalvessilva@outlook.com.br>
2021-02-17 01:58:33 -0300

    Update Auth Middleware
    New function added: now, there is a function which will check if user is authenticated, and if is already logged.

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,5 +1,14 @@
-exports.alreadyLogged = async (req,res,next)=>{
+exports.isAuthenticated = async (req,res,next)=>{
     if(!req.session.email)
         return res.status('403').redirect('/');
+
+    res.locals.email = req.session.email;
+    next();
+};
+exports.AlreadyLogged = async (req,res,next)=>{
+    if(req.session.email){
+        res.locals.email = req.session.email;
+        return res.redirect('/profile');
+    }
     next();
 };
