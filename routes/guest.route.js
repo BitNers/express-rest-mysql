b013043dd3c93037b479a1efdb7b2ae1381a136b
BitNers <wesleyalvessilva@outlook.com.br>
2021-02-17 01:56:42 -0300

    Updating Routes
    Adding new middlewares and rename home to Guest route.

---
+++ routes/guest.route.js
@@ -0,0 +1,12 @@
+module.exports = (app)=>{
+    const guestControl = require("../controllers/guest.controller");
+    const auth = require("../middleware/auth.middleware");
+    // Access Home Page
+    app.route('/')
+    .get(async (req,res)=>{res.render('pages/home', {data:{title:"Home"}})})
+    .post(guestControl.createUser)
+    
+    app.route('/login')
+    .get(auth.AlreadyLogged,async (req,res)=>{res.render('pages/Login', {data:{title:"Login"}})})
+    .post(guestControl.validateLogin);
+}
--- routes/user.route.js
+++ routes/user.route.js
@@ -2,7 +2,8 @@ module.exports = (app)=>{
     
     const userControl = require("../controllers/user.controller");
     const Auth = require('../middleware/auth.middleware');
-    app.use('/profile', Auth.alreadyLogged);
+
+    app.use('/profile', Auth.isAuthenticated);
     
     app.get('/profile/logout', userControl.logout);
     app.get('/profile', userControl.getInfo);
