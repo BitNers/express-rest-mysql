48b5cabb52d242c59e80a1e59f7ffcbc5ffd13c3
BitNers <wesleyalvessilva@outlook.com.br>
2021-02-20 15:14:30 -0300

    Updating Bunker Controller
    Adding basic sequelize query to return user data to Bunker View.

--- controllers/bunker.controller.js
+++ controllers/bunker.controller.js
@@ -4,5 +4,13 @@ const db = require('../database/models');
 const User = db.User;
 
 exports.getBunker = async (req,res)=>{
-   res.render('pages/bunker/home', {layout:false});
+   await User.findOne({
+      where: {email: req.session.email}, 
+      attributes: {
+          exclude: ['passwd']}})
+       .then(data=>{
+         res.render('pages/bunker/home', {layout:false, data: {title: "Bunker", data:data}});
+       })
+       .catch(err=>{res.status(500).json({status: "error", message: "Something went wrong while try to find user by name."})});
+
 }
