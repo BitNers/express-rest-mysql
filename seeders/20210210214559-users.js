ddadd135cc950a813a1c53ab894b5644f2999069
BitNers <wesleyalvessilva@outlook.com.br>
2021-04-02 22:40:56 -0300

    Updating User Seeder
    Removing some unused seeders.

--- seeders/20210210214559-users.js
+++ seeders/20210210214559-users.js
@@ -10,21 +10,6 @@ module.exports = {
       passwd: '$2b$08$J1GgFmkGMZME/NMVnPveSu9IMGOpTGoscnXanFrpqv1GhgNNf8Wei',
     },
     {
-      username: 'Johny Johny',
-      email: 'test2@medium.com',
-      passwd: '$2b$08$J1GgFmkGMZME/NMVnPveSu9IMGOpTGoscnXanFrpqv1GhgNNf8Wei',
-    },
-    {
-      username: 'John Wick',
-      email: 'john@wick.com.br',
-      passwd: '$2b$08$J1GgFmkGMZME/NMVnPveSu9IMGOpTGoscnXanFrpqv1GhgNNf8Wei',
-    },
-    {
-      username: 'Joker Arthur',
-      email: 'j0ker@arthur.ch',
-      passwd: '$2b$08$J1GgFmkGMZME/NMVnPveSu9IMGOpTGoscnXanFrpqv1GhgNNf8Wei',
-    },
-    {
       username: 'Wesley',
       email: 'adedonha@adedonha.com.br',
       passwd: '$2b$08$gS1eH2ZmcW8/b8Lmo5ueT.u2yyuKvSSVKf3lb5LXkBZfjipvB3Uzu',
