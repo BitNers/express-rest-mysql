ee2f53b5d3c5e29bc7cec81e3f2376f7e648ba75
BitNers <wesleyalvessilva@outlook.com.br>
2021-04-02 22:36:48 -0300

    Updating User Controller
    Commenting unused modules and functions, updating Data to return a beauty date.

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,12 +1,18 @@
 const db = require('../database/models');
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 const MaskData = require('maskdata');
 const getSize = require('get-folder-size');
 const User = db.User;
 
-function decodeJwt(txt){
+/* function decodeJwt(txt){
   return jwt.verify(txt, process.env.ACCESS_TOKEN_SECRET)
 }
+
+exports.findAllUsers = async (res) => {
+    await User.findAll({attributes: {exclude: ['passwd']}, limit:250})
+    .then(users=>{res.json(users);})
+    .catch(err=>{res.status(500).json({status: "error", message: err || "Something went wrong while try to find all users."})})
+ };*/
 
 const MaskEmailOption = {
   maskWith: "*", 
@@ -22,32 +28,28 @@ exports.logout = async (req,res)=>{
   return res.redirect('/');
 }
 
-exports.findAllUsers = async (req, res) => {
-    await User.findAll({attributes: {exclude: ['passwd']}, limit:250})
-    .then(users=>{res.json(users);})
-    .catch(err=>{res.status(500).json({status: "error", message: err || "Something went wrong while try to find all users."})})
- };
+
+
 
 exports.getInfo = async (req,res)=>{
-
   await User.findOne({
          where: {email: req.session.email}, 
-         attributes: {
-             exclude: ['passwd']}})
-          .then(data=>{
+         attributes: {exclude: ['passwd','passwd_bunker']}
+        })
+         .then(data=>{
             data.email = MaskData.maskEmail2(data.email, MaskEmailOption);
-
+            data.setDataValue("createdAt",  data.createdAt.toLocaleDateString("pt-BR"));
             getSize(data.path_bunker, (err,size)=>{
               if(!err){
                 let fSize = (size /(1024**3)).toFixed(2);
                 let fPercent = ((fSize/process.env.MAX_FREE_SPACE)*100).toFixed(0);
-
                 data.setDataValue("sizeFolder", {usedSpace: fSize, percentSpace: fPercent});
-
                 res.render('pages/profile/home', {data:{title: "Profile", data: data}});
+              }else{
+                console.log(`Something went wrong while try to get user folder size: ${err}`);
               }
             });
             
           })
-          .catch(err=>{res.status(500).json({status: "error", message: "Something went wrong while try to find user by name."})});
+          .catch(err=>{res.status(500).json({status: "error", message: `Something went wrong while try to find user by name: ${err}`})});
 }
