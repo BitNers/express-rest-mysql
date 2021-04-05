8b4b0b3aa3622f73143aee971c89a82433a4e63a
BitNers <wesleyalvessilva@outlook.com.br>
2021-04-05 09:27:26 -0300

    Creating Issue Model
    Issues Model.

---
+++ models/issue.model.js
@@ -0,0 +1,40 @@
+module.exports = (sequelize, Sequelize) =>{
+  const Issue = sequelize.define('issues',{
+    id_issue: {
+      type: Sequelize.INTEGER, 
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+      unsigned: true
+  },
+
+  category:{
+    type: Sequelize.ENUM('Bunker', 'Profile', 'Home Page', 'Feedback', 'Others'),
+    allowNull: false,
+  },
+
+  title: {
+      type: Sequelize.STRING,
+      allowNull: false
+  },
+
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate:{
+      isEmail: true
+    }
+},
+  createdAt:{
+    type: 'TIMESTAMP',
+    defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+    allowNull: false
+  },
+
+  });
+  return Issue;
+};
