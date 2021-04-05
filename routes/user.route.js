78dd99cb9e18e99fc4baa8d4ec5040746ae6d1ae
BitNers <wesleyalvessilva@outlook.com.br>
2021-04-05 09:27:55 -0300

    Update User Route
    Adding new paths for User, to update password & E-mail.

--- routes/user.route.js
+++ routes/user.route.js
@@ -7,4 +7,6 @@ module.exports = (app)=>{
     
     app.get('/profile', userControl.getInfo);
     app.get('/profile/logout', userControl.logout);
+    app.post('/profile/updatePassword/:email', userControl.updatePassword);
+    app.patch('/profile/updateEmail/:email', userControl.updateEmail);
 }
